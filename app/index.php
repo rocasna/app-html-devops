989be7acb269ed3feb7ceba208de017a6310488e
Tu Nombre <rober@example.com>
2025-05-22 21:27:58 +0200

    subimos cambio en la app y cambio en el configmap de nginx

--- app/index.php
+++ app/index.php
@@ -1,5 +1,53 @@
 <?php
+echo '
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenidos a mi página web</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 0;
+            text-align: center;
+        }
+        main {
+            padding: 20px;
+            text-align: center;
+        }
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px 0;
+            position: fixed;
+            width: 100%;
+            bottom: 0;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Bienvenidos a mi página web</h1>
+    </header>
 
-phpinfo( );
+    <main>
+        <p>Versión de la app: v1.1</p>
+    </main>
 
+    <footer>
+        <p>&copy; 2025 - DevOps </p>
+    </footer>
+</body>
+</html>
+
+';
 ?>
